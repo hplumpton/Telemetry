31fabca51b7f45f2a7ea76a6ccb5866a069be983
plumptonhannah <plumptonhannah@gmail.com>
2017-10-18 11:56:32 -0400

    attempting to figure out home range code

--- telemetry.R
+++ telemetry.R
@@ -10,4 +10,16 @@ library(maptools)
 tel<-read.csv("TelData2.csv")
 coordinates(tel)<-c("longitud","latitude")
 proj4string(tel)<-CRS("+proj=longlat +datum=WGS84") 
-writeOGR(obj=tel, dsn="tempdir", layer="telemetry", driver="ESRI Shapefile")
+#writeOGR(obj=tel, dsn="tempdir", layer="telemetry", driver="ESRI Shapefile")
+
+
+b36485 <- subset(tel, ptt=="36485", 
+                  select=c(animal:sampsize))
+coordinates(b36485)<-c("longitud","latitude")
+proj4string(b36485)<-CRS("+proj=longlat +datum=WGS84")
+
+library(adehabitatHR)
+home<-kernelUD(b36485[,3], h="LSCV",hlim= )
+image(home)
+ver<-getverticeshr(home,95)
+plot(ver)
