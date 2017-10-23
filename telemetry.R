e8e3a8d5df59e8f9a8b0dfc82896c250ec1f424d
plumptonhannah <plumptonhannah@gmail.com>
2017-10-23 15:44:54 -0400

    subsetting data and figuring out kernel density

--- telemetry.R
+++ telemetry.R
@@ -13,13 +13,20 @@ proj4string(tel)<-CRS("+proj=longlat +datum=WGS84")
 #writeOGR(obj=tel, dsn="tempdir", layer="telemetry", driver="ESRI Shapefile")
 
 
-b36485 <- subset(tel, ptt=="36485", 
-                  select=c(animal:sampsize))
-coordinates(b36485)<-c("longitud","latitude")
-proj4string(b36485)<-CRS("+proj=longlat +datum=WGS84")
+
+b26486f10<-rgdal::readOGR("tempdir/blsc26486f11.shp")
+b26486f10<-subset(b26486f10,lat_p>44.5 & lon_p<81.5,select=animal:sampsiz)
+L1<-subset(b26486f10,lc94=="L1",select=animal:sampsiz)
+L2<-subset(b26486f10,lc94=="L2",select=animal:sampsiz)
+L3<-subset(b26486f10,lc94=="L3",select=animal:sampsiz)
+b26486f10<-rbind(L1,L2,L3)
+summary(b26486f10)
+
+b80908<-rgdal::readOGR("tempdir/blsc80908.shp")
+b80908<-spTransform(b80908,CRS(proj4string(tel)))
 
 library(adehabitatHR)
-home<-kernelUD(b36485[,3], h="LSCV",hlim= )
+home<-kernelUD(b80908[,3], h="href",grid=50, extent=0.5)
 image(home)
 ver<-getverticeshr(home,95)
 plot(ver)
