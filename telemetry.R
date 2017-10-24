ed0382abbe55c97460d30841fbdec4f1160010dd
plumptonhannah <plumptonhannah@gmail.com>
2017-10-24 14:25:24 -0400

    coding

--- telemetry.R
+++ telemetry.R
@@ -20,6 +20,7 @@ L1<-subset(b26486f10,lc94=="L1",select=animal:sampsiz)
 L2<-subset(b26486f10,lc94=="L2",select=animal:sampsiz)
 L3<-subset(b26486f10,lc94=="L3",select=animal:sampsiz)
 b26486f10<-rbind(L1,L2,L3)
+b26486f10<-spTransform(b26486f10,CRS(proj4string(tel)))
 summary(b26486f10)
 
 b80908<-rgdal::readOGR("tempdir/blsc80908.shp")
@@ -28,5 +29,9 @@ b80908<-spTransform(b80908,CRS(proj4string(tel)))
 library(adehabitatHR)
 home<-kernelUD(b80908[,3], h="href",grid=50, extent=0.5)
 image(home)
-ver<-getverticeshr(home,95)
+ver<-getverticeshr(home,50)
 plot(ver)
+
+library(ks)
+
+hb26486f10<-kde(b26486f10)
